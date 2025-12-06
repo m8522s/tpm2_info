f5ce970a9f72f613c8fedbc869dc3dffaf68d7fe
m8522s <m8522s.github@stubbig.com>
2025-12-06 21:53:05 +0100

    Check if required commands exist

--- tpm2_info.sh
+++ tpm2_info.sh
@@ -8,7 +8,14 @@
 MODULE=/usr/lib64/pkcs11/libtpm2_pkcs11.so
 
 
-# Check for commands
+# Check if required commands exist
+if ! command -v tpm2_getcap >/dev/null 2>&1 ; then
+  echo "tpm2-tools not installed"
+  exit 1
+elif ! command -v pkcs11-tool >/dev/null 2>&1 ; then
+  echo "opensc not installed"
+  exit 2
+fi
 
 
 # Get capabilities
